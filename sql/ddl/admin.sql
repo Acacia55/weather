fe6b53645d76592cf251952886962269e0846d70
Acacia55 <135031075+Acacia55@users.noreply.github.com>
2024-06-14 10:05:11 +0800

    Update admin.sql

--- sql/ddl/admin.sql
+++ sql/ddl/admin.sql
@@ -1,1 +1,8 @@
-
+create table if not exists shixun.admin
+(
+    id       int          not null comment '管理员ID，唯一身份标识'
+        primary key,
+    code     varchar(20)  null comment '登录编码',
+    password varchar(20)  null comment '登录密码',
+    remarks  varchar(255) null comment '备注'
+);
